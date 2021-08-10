379bcb27998c3d030140fc26a98adf0cfb892a6b
faisalwilmarm <faisalwilmar@gmail.com>
2021-08-10 20:34:50 +0700

    feat: finished first step of learning

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# task-planner
+# Project Title
+
+The most common way to practice building RESTful API is a Task Planner. This is it.
+
+## Lessons Learned
+
+This project is based on Nic Raboy's Article in [here]("https://www.thepolyglotdeveloper.com/2019/02/developing-restful-api-golang-mongodb-nosql-database/")
+
+When following his article using Visual Studio Code in windows, the intellisense said that required module is missing. I fix that by initiating go module before started to working on the project.
+```bash
+go mod init task-planner
+```
+Everytime the intellisense says the module is missing, or the required dependency is incomplete, just run go mod tidy.
+```bash
+go mod tidy
+```
+
+  
+## Installation
+
+Clone this project.
+Make sure you have Golang >1.6.6 installed and a MongoDB Cluster Database
+
+    
---
+++ main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Task struct {
+	ID          primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Title       string             `json:"title,omitempty" bson:"title,omitempty"`
+	Description string             `json:"description,omitempty" bson:"description,omitempty"`
+}
+
+var client *mongo.Client
+
+func CreateTaskEndpoint(response http.ResponseWriter, req *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	var task Task
+
+	_ = json.NewDecoder(req.Body).Decode(&task)
+	collection := client.Database("task-planner").Collection("task")
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+
+	result, err := collection.InsertOne(ctx, task)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{"message":" "` + err.Error() + `"}`))
+		return
+	}
+
+	json.NewEncoder(response).Encode(result)
+}
+
+func GetTasksEndpoint(response http.ResponseWriter, req *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	var tasks []Task
+
+	collection := client.Database("task-planner").Collection("task")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
+	// we need to retrieve all data
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{"message":" "` + err.Error() + `"}`))
+		return
+	}
+
+	// close the cursor
+	defer cursor.Close(ctx)
+
+	// loop for each item in cursor, decode and insert it to Slice
+	for cursor.Next(ctx) {
+		var task Task
+		cursor.Decode(&task)
+		tasks = append(tasks, task)
+	}
+
+	if err := cursor.Err(); err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{"message":" "` + err.Error() + `"}`))
+		return
+	}
+
+	json.NewEncoder(response).Encode(tasks)
+}
+
+func GetSingleTaskEndpoint(response http.ResponseWriter, req *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	params := mux.Vars(req)
+	id, _ := primitive.ObjectIDFromHex(params["id"])
+
+	var task Task
+
+	collection := client.Database("task-planner").Collection("task")
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+
+	err := collection.FindOne(ctx, Task{ID: id}).Decode(&task)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{"message": "` + err.Error() + `"}`))
+		return
+	}
+
+	json.NewEncoder(response).Encode(task)
+}
+
+func main() {
+	fmt.Println("Starting the application...")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	clientOptions := options.Client().ApplyURI("mongodb+srv://taskuser:7EHTZNXRJHCNokW2@task-planner.gbyx1.mongodb.net/task-planner?retryWrites=true&w=majority")
+	localClient, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	client = localClient
+	router := mux.NewRouter()
+	router.HandleFunc("/task", CreateTaskEndpoint).Methods("POST")
+	router.HandleFunc("/task/{id}", GetSingleTaskEndpoint).Methods("GET")
+	router.HandleFunc("/task", GetTasksEndpoint).Methods("GET")
+	http.ListenAndServe(":12345", router)
+}
+
+// reference https://www.thepolyglotdeveloper.com/2019/02/developing-restful-api-golang-mongodb-nosql-database/
